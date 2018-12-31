bc4b0a65fed004f5008cf1c010279f3bd0079f14
Lewis <zlwcoder@126.com>
2018-12-31 07:55:09 +0000

    管理员删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -79,4 +79,12 @@ class UsersController extends Controller
         $users = User::paginate(6);
         return view('users.index', compact('users'));
     }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,13 +9,13 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
     public function update(User $currentUser, User $user)
     {
         return $currentUser->id === $user->id;
+    }
+
+     public function destroy(User $currentUser, User $user)
+    {
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
 }

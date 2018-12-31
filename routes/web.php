68b0884a926b22fdc2787e192081ee37b939d809
Lewis <zlwcoder@126.com>
2018-12-31 12:58:19 +0000

    完成关注与取消关注功能

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,6 @@ Route::post('/statuses','StatusesController@store')->name('statuses.store');
 
 Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
 Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');

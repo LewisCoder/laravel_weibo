49e2a372452d3416ddbf5736c04cb7f0efca5074
Lewis <zlwcoder@126.com>
2018-12-31 07:26:49 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,12 +7,24 @@ use Auth;
 
 class SessionsController extends Controller
 {
+
+  public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+    
     public function create()
     {
         return view('sessions.create');
     }
 
-    public function store(Request $request)
+   public function store(Request $request)
     {
        $credentials = $this->validate($request, [
            'email' => 'required|email|max:255',
@@ -21,7 +33,8 @@ class SessionsController extends Controller
 
        if (Auth::attempt($credentials, $request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
+           $fallback = route('users.show', Auth::user());
+           return redirect()->intended($fallback);
        } else {
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back()->withInput();

0ac6899da36afca9142133d7bdc822bc08da9a3c
Lewis <zlwcoder@126.com>
2018-12-31 02:21:53 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', '主页')
+
+@section('content')
+  <h1>主页2</h1>
+@stop
